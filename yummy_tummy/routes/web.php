9ffd7a8207477e9ddfefaa9a5d2d483665638773
Hunter-420 <khanalnischal2075@gmail.com>
2023-10-15 11:55:21 +0545

    login

--- yummy_tummy/routes/web.php
+++ yummy_tummy/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\AdminLoginController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// route admin/AdminLoginController
+Route::get('/admin/login', [AdminLoginController::class,'index'])->name('admin.login');
